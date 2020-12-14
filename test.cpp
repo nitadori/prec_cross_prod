0754c199ddbeafa7164cbff4401d6b1e4e74723b
nitadori <keigo@riken.jp>
2020-12-14 17:13:51 +0900

    inline, misc

--- test.cpp
+++ test.cpp
@@ -29,16 +29,16 @@ struct Vec3{
 
 // a * b + c without intermediate rounding
 template <typename F>
-F tp_fma(F a, F b, F c);
+inline F tp_fma(F a, F b, F c);
 
 template<>
-float tp_fma(float a, float b, float c){ return ::fmaf(a, b, c); }
+inline float tp_fma(float a, float b, float c){ return ::fmaf(a, b, c); }
 
 template<>
-double tp_fma(double a, double b, double c){ return ::fma(a, b, c); }
+inline double tp_fma(double a, double b, double c){ return ::fma(a, b, c); }
 
 template <typename F>
-F prec_cross_prod_inner(F ux, F uy, F vx, F vy){
+inline F prec_cross_prod_inner(F ux, F uy, F vx, F vy){
 	F p = ux * vy;
 	F r = tp_fma(ux, vy, -p);
 	return r + tp_fma(-vx, uy, p);
@@ -76,43 +76,48 @@ int vec_print(
 	return fprintf(fp, form, v.x, v.y, v.z);
 }
 
-int main(){
-	srand48(20201212);
+int main(int ac, char **av){
+	srand48(ac>1 ? atof(av[1]) : 20201212);
 
 	Vec3<double> u, v;
 
-	auto rnd = drand48; // ごめんなさい
+	auto rnd = []{
+		return drand48() - 0.5;
+	};
 
-	u.x = rnd()*rnd();
-	u.y = rnd()*rnd();
-	u.z = rnd()*rnd();
+	u.x = rnd() + 0.5;
+	u.y = rnd() + 0.5;
+	u.z = rnd() + 0.5;
 
-	const double eps = 1.e-7;
+	const double eps = 1.0e-7;
 
 	// 微小回転でほぼ平行なベクトルを作る
 	v.x = u.x + eps * (rnd() * u.y + rnd() * u.z);
 	v.y = u.y + eps * (rnd() * u.z + rnd() * u.x);
 	v.z = u.z + eps * (rnd() * u.x + rnd() * u.y);
 
-	Vec3<float>  uf = u, vf = v;
+	Vec3<float>  uf = u,  vf = v;
 	Vec3<double> ud = uf, vd = vf;
 
-	auto wf0 = cross_prod(uf, vf);
-	auto wf  = prec_cross_prod(uf, vf);
-	auto wd  = cross_prod(ud, vd);
-	auto wd1 = prec_cross_prod(ud, vd);
+	auto wf0 = cross_prod      (uf, vf);
+	auto wf  = prec_cross_prod (uf, vf);
+	auto wd  = cross_prod      (ud, vd);
+	auto wd1 = prec_cross_prod (ud, vd);
+	auto wd2=  prec_cross_prod (u,  v ); // 入力を単精度に丸める前のもの
 
 	const char *form1 =  "(%24.16e, %24.16e, %24.16e)\n";
 	vec_print(wf0, form1);
 	vec_print(wf,  form1);
 	vec_print(wd,  form1);
 	vec_print(wd1, form1);
+	vec_print(wd2, form1);
 
 	const char *form2 =  "(%A, %A, %A)\n";
 	vec_print(wf0, form2);
 	vec_print(wf,  form2);
 	vec_print(wd,  form2);
 	vec_print(wd1, form2);
+	vec_print(wd2, form2);
 
 	return 0;
 }
